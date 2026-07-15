ea5aef682360408379b474d104ca8126d4099da8
Jordan Castro <jordan.castro@empire.ca>
2026-07-15 18:25:10 -0400

    set laptop IP, complete network test

--- scripts/network_test.gd
+++ scripts/network_test.gd
@@ -1,5 +1,6 @@
 extends Control
 
+const MAC_IP = "192.168.2.23"
 
 func _on_host_button_pressed():
 	print("host btn pressed")
@@ -8,4 +9,4 @@ func _on_host_button_pressed():
 
 func _on_join_button_pressed():
 	print("join btn pressed")
-	NetworkManager.join_game("192.168.1.25")
+	NetworkManager.join_game(MAC_IP)
