7349f1769534ddda53119088bfc892d4662249e3
Jordan Castro <jordan.castro@empire.ca>
2026-07-15 22:35:18 -0400

    fix animation

--- scripts/monkey.gd
+++ scripts/monkey.gd
@@ -4,11 +4,13 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var monkey = $AnimatedSprite2D
-var current_animation: String = "standing-front"
+@export var current_animation: String = "standing-front"
 
 func _ready():
 	if not is_multiplayer_authority():
-		set_physics_process(false)
+		#set_physics_process(false)
+		if monkey.animation != current_animation:
+			monkey.play(current_animation)
 
 func _physics_process(delta: float) -> void:		
 	var direction := Input.get_vector("walk-left", "walk-right", "walk-backward", "walk-forward")
@@ -22,32 +24,37 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
+func change_animation(anim_name: String):
+	current_animation = anim_name
+	if monkey.animation != anim_name:
+		monkey.play(anim_name)
+	
 func update_walk_animation(dir: Vector2) -> void:
 	# diagonal up-right
 	if dir.x > 0 and dir.y > 0:
-		monkey.play("walking-front-diagonal")
+		change_animation("walking-front-diagonal")
 		monkey.flip_h = false
 	# diagonal down-right
 	elif dir.x > 0 and dir.y < 0:
-		monkey.play("walking-back-diagonal")
+		change_animation("walking-back-diagonal")
 		monkey.flip_h = false
 	# diagonal up-left
 	elif dir.x < 0 and dir.y > 0:
-		monkey.play("walking-front-diagonal")
+		change_animation("walking-front-diagonal")
 		monkey.flip_h = true
 	# diagonal down-left
 	elif dir.x < 0 and dir.y < 0:
-		monkey.play("walking-back-diagonal")
+		change_animation("walking-back-diagonal")
 		monkey.flip_h = true
 	# only vertical
 	elif dir.y != 0:
 		if dir.y > 0:
-			monkey.play("walking-front")
+			change_animation("walking-front")
 		else:
-			monkey.play("walking-back")
+			change_animation("walking-back")
 		monkey.flip_h = false
 	# only horizontal
 	elif dir.x != 0:
-		monkey.play("walking-side")
+		change_animation("walking-side")
 		monkey.flip_h = (dir.x < 0)
 	
