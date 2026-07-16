ec359942d455a18b13412b6acce821f9e74217a8
Jordan Castro <jordan.castro@empire.ca>
2026-07-15 22:09:33 -0400

    setup for network test with jungle plaza

--- scripts/network_test.gd
+++ scripts/network_test.gd
@@ -1,12 +1,15 @@
 extends Control
 
 const MAC_IP = "192.168.2.23"
+const GAME_SCENE = "res://scenes/jungle_plaza.tscn"
 
 func _on_host_button_pressed():
 	print("host btn pressed")
 	NetworkManager.host_game()
+	get_tree().change_scene_to_file(GAME_SCENE)
 
 
 func _on_join_button_pressed():
 	print("join btn pressed")
 	NetworkManager.join_game(MAC_IP)
+	get_tree().change_scene_to_file(GAME_SCENE)
