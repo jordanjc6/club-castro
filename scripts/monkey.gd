8bb48b9ec33219e2340275a5ef79b0e45c3b41d5
Jordan Castro <jordan.castro@empire.ca>
2026-07-16 21:35:40 -0400

    Revert "set up project for testing basic LAN multiplayer connection"
    This reverts commit 8231473d589055eb51c2a502007b21cac0fd44e9.
    
    Revert to before starting any networking/multiplayer

--- scripts/monkey.gd
+++ scripts/monkey.gd
@@ -5,72 +5,75 @@ const JUMP_VELOCITY = -400.0
 
 @onready var monkey = $AnimatedSprite2D
 
-@export var synced_animation: String = "standing-front"
-@export var synced_flip: bool = false
-
-#func _ready():
-	#if not is_multiplayer_authority():
-		##set_physics_process(false)
-		#if monkey.animation != synced_animation:
-			#monkey.play(synced_animation)
-
 func _physics_process(delta: float) -> void:
-	if is_multiplayer_authority():
-		var direction := Input.get_vector("walk-left", "walk-right", "walk-backward", "walk-forward")
-		
-		if direction != Vector2.ZERO:
-			velocity = direction * SPEED
-			update_walk_animation(direction)
-		else:
-			velocity = velocity.move_toward(Vector2.ZERO, SPEED)
-			set_monkey_animation("standing-front")
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 
-		move_and_slide()
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	#var direction := Input.get_axis("walk-left", "walk-right")
+	#
+	#if direction:
+		#velocity.x = direction * SPEED
+		#
+		## ONLY play the walking animation if moving right (positive direction)
+		#if direction > 0:
+			#if monkey.animation != "walking-side":
+				#monkey.play("walking-side")
+				#monkey.flip_h = false
+		#else:
+			## If moving left, you can play a left animation or fall back to idle
+			#if monkey.animation != "walking-side":
+				#monkey.play("walking-side")
+				#monkey.flip_h = true
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		## Return to idle animation when no input keys are pressed
+		#if monkey.animation != "standing-front":
+			#monkey.play("standing-front")
+			#monkey.flip_h = false
+			
+	var direction := Input.get_vector("walk-left", "walk-right", "walk-backward", "walk-forward")
 	
-	
-func set_monkey_animation(anim_name:String):
-	synced_animation = anim_name
-	if monkey.animation != anim_name:
-		monkey.play(anim_name)
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		update_walk_animation(direction)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		monkey.stop()
+
+	move_and_slide()
 	
 func update_walk_animation(dir: Vector2) -> void:
 	# diagonal up-right
 	if dir.x > 0 and dir.y > 0:
-		set_monkey_animation("walking-front-diagonal")
-		synced_flip = false
+		monkey.play("walking-front-diagonal")
 		monkey.flip_h = false
 	# diagonal down-right
 	elif dir.x > 0 and dir.y < 0:
-		set_monkey_animation("walking-back-diagonal")
-		synced_flip = false
+		monkey.play("walking-back-diagonal")
 		monkey.flip_h = false
 	# diagonal up-left
 	elif dir.x < 0 and dir.y > 0:
-		set_monkey_animation("walking-front-diagonal")
-		synced_flip = true
+		monkey.play("walking-front-diagonal")
 		monkey.flip_h = true
 	# diagonal down-left
 	elif dir.x < 0 and dir.y < 0:
-		set_monkey_animation("walking-back-diagonal")
-		synced_flip = true
+		monkey.play("walking-back-diagonal")
 		monkey.flip_h = true
 	# only vertical
 	elif dir.y != 0:
 		if dir.y > 0:
-			set_monkey_animation("walking-front")
+			monkey.play("walking-front")
 		else:
-			set_monkey_animation("walking-back")
-		synced_flip = false
+			monkey.play("walking-back")
 		monkey.flip_h = false
 	# only horizontal
 	elif dir.x != 0:
-		set_monkey_animation("walking-side")
-		synced_flip = (dir.x < 0)
+		monkey.play("walking-side")
 		monkey.flip_h = (dir.x < 0)
-		
-func _process(delta):
-	if not is_multiplayer_authority():
-		if monkey.animation != synced_animation:
-			monkey.play(synced_animation)
-
-		monkey.flip_h = synced_flip
+	
