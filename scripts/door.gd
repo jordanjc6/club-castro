48066141e9b934b3078acd5c9dfc4e7b7c590537
Jordan Castro <jordan.castro@empire.ca>
2026-07-17 14:48:03 -0400

    can enter doors as host (updated collision layers/masks of map barrier, character, and doors)

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export_file("*.tscn") var target_room: String
 
 func _on_body_entered(body: Node2D) -> void:
+	print("door entered by %s" % body)
 	# Ensure the thing touching the door is actually the player
 	if body is CharacterBody2D and target_room != "":
 		SceneManager.switch_scene(target_room)
