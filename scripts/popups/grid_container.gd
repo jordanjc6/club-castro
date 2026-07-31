cbfa4cdbd948e1b57a6b906ffa67a7c8423a17c1
Jordan Castro <jordanjcastro8@gmail.com>
2026-07-30 23:47:45 -0400

    basic style for tictactoe game

--- scripts/popups/grid_container.gd
+++ scripts/popups/grid_container.gd
@@ -6,9 +6,14 @@ func _ready() -> void:
 		var btn = get_child(i) as Button
 		
 		# Clear any default text and make them square block sizes
-		btn.text = ""
-		btn.custom_minimum_size = Vector2(80, 80) 
-		
+		#btn.text = ""
+		#btn.custom_minimum_size = Vector2(80, 80) 
+		# 1. Change the Font Size (e.g., set to 48px)
+		#btn.add_theme_font_size_override("font_size", 72)
+	
+		# 2. Change the Font Color (Normal state)
+		#btn.add_theme_color_override("font_color", Color.BLACK)
+	
 		# Connect the click event dynamically
 		btn.pressed.connect(func():
 			# Path: GridContainer -> VBox/Center -> VBoxContainer -> GameWindow -> MinigameUI -> MinigameArea
