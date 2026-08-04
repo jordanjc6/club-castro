cece781bce2123fb6116fcadc13560850f431d76
Jordan Castro <jordanjcastro8@gmail.com>
2026-08-04 17:18:49 -0400

    enable host/join from anywhere in the game, ensure theatre count updates for spawns/disconnects

--- scripts/world/multiplayer_manager.gd
+++ scripts/world/multiplayer_manager.gd
@@ -4,6 +4,13 @@ const SERVER_PORT = 8080
 const SERVER_IP = "127.0.0.1"
 const MAC_IP = "192.168.2.23"
 
+# (x1, y1) = top left theatremap offset
+# (x2, y2) = bottom right theatremap based on texturerect size (1280, 720)
+const THEATRE_X1 = 0
+const THEATRE_X2 = 1280 
+const THEATRE_Y1 = -1800
+const THEATRE_Y2 = -1800 + 720
+
 var multiplayer_scene = preload("res://scenes/player/monkey_multiplayer.tscn")
 var _players_spawn_node
 var host_mode_enabled = false
@@ -15,12 +22,9 @@ func become_host():
 	print("become host")
 	host_mode_enabled = true
 	
-	# move multiplayers spawn point to current position of single player
+	# set players spawn node
 	var world_scene = get_tree().get_current_scene()
 	_players_spawn_node = world_scene.get_node("Players")
-	var single_player = world_scene.get_node_or_null("SinglePlayer")
-	if single_player:
-		_players_spawn_node.global_position = single_player.global_position
 	
 	# create multiplayer server
 	var server_peer = ENetMultiplayerPeer.new()
@@ -31,9 +35,12 @@ func become_host():
 	multiplayer.peer_connected.connect(_add_player_to_game)
 	multiplayer.peer_disconnected.connect(_delete_player)
 	
-	# remove single player and spawn multi player
+	# spawn multi player and remove single player
+	var single_player = world_scene.get_node_or_null("SinglePlayer")
+	var position = single_player.global_position
+	var offset = single_player.current_grid_offset
+	_add_player_to_game(1, position, offset)
 	_remove_single_player()
-	_add_player_to_game(1)
 
 func join_game():
 	print("join game")
@@ -46,18 +53,32 @@ func join_game():
 	
 	_remove_single_player()
 	
-func _add_player_to_game(id: int):
+func _add_player_to_game(id: int, position: Vector2 = Vector2.INF, offset: Vector2 = Vector2.INF):
 	print("player %s joined the game" % id)
 	
+	# instantiate player
 	var player_to_add = multiplayer_scene.instantiate()
 	player_to_add.player_id = id
 	player_to_add.name = str(id)
-	
-	# Add child first so multiplayer authority & spawner register it
 	_players_spawn_node.add_child(player_to_add, true)
 	
-	# If a client joined an active host (Host is peer ID 1)
+	# set player spawn position
+	if id == 1:
+		# for host
+		player_to_add.global_position = position
+		player_to_add.current_grid_offset = offset
+		
+		# Tell the client explicitly to update its zone offset
+		player_to_add.rpc("update_zone_offset", offset)
+		
+		# increment count if player spawns in theatre
+		var x = position.x
+		var y = position.y
+		if (x >= THEATRE_X1 and x <= THEATRE_X2 and y >= THEATRE_Y1 and y <= THEATRE_Y2):
+			MultiplayerManager.rpc_id(1, "increment_players_in_theatre")
+	
 	if id != 1:
+		# for client
 		var host_player = _players_spawn_node.get_node_or_null("1")
 		if host_player:
 			var target_pos = host_player.global_position
@@ -71,26 +92,39 @@ func _add_player_to_game(id: int):
 			player_to_add.rpc("update_zone_offset", target_grid_offset)
 			
 			# increment count if player spawns in theatre
-			# (x1, y1) = top left theatremap offset
-			# (x2, y2) = bottom right theatremap based on texturerect size (1280, 720)
-			var theatre_x1 = 0
-			var theatre_x2 = 1280 
-			var theatre_y1 = -1800
-			var theatre_y2 = -1800 + 720
 			var x = target_pos.x
 			var y = target_pos.y
-			if (x >= theatre_x1 and x <= theatre_x2 and y >= theatre_y1 and y <= theatre_y2):
+			if (x >= THEATRE_X1 and x <= THEATRE_X2 and y >= THEATRE_Y1 and y <= THEATRE_Y2):
 				MultiplayerManager.rpc_id(1, "increment_players_in_theatre")
-	
+
 func _delete_player(id: int):
 	print("player %s left the game" % id)
 	if not (_players_spawn_node.has_node(str(id))):
 		return
+	var player = _players_spawn_node.get_node(str(id))
+	
+	# decrement theatre players if removed player was in there
+	var player_position = player.global_position
+	var x = player_position.x
+	var y = player_position.y
+	if (x >= THEATRE_X1 and x <= THEATRE_X2 and y >= THEATRE_Y1 and y <= THEATRE_Y2):
+		MultiplayerManager.rpc_id(1, "decrement_players_in_theatre")
+	
+	# remove player
 	_players_spawn_node.get_node(str(id)).queue_free()
 	
 func _remove_single_player():
 	print("remove single player")
 	var player_to_remove = get_tree().get_current_scene().get_node("SinglePlayer")
+	
+	# decrement theatre players if removed player was in there
+	var player_position = player_to_remove.global_position
+	var x = player_position.x
+	var y = player_position.y
+	if (x >= THEATRE_X1 and x <= THEATRE_X2 and y >= THEATRE_Y1 and y <= THEATRE_Y2):
+		MultiplayerManager.rpc_id(1, "decrement_players_in_theatre")
+	
+	# remove player
 	player_to_remove.queue_free()
 
 @rpc("any_peer", "call_local", "reliable")
