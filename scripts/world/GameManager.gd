8bca64d81e1e3e1366427fd997d613ff8f3c7b43
Jordan Castro <jordanjcastro8@gmail.com>
2026-08-04 23:54:34 -0400

    style multiplayer HUD, make it collapsible

--- scripts/world/GameManager.gd
+++ scripts/world/GameManager.gd
@@ -1,13 +1,20 @@
 extends Node
 
+@onready var host_button: Button = $"../HUD/SideNav/MultiplayerHUD/VBoxContainer/HostButton"
+@onready var join_button: Button = $"../HUD/SideNav/MultiplayerHUD/VBoxContainer/JoinButton"
+
+
+func _ready() -> void:
+	host_button.pressed.connect(_host_button_pressed)
+	join_button.pressed.connect(_join_button_pressed)
 
 func _host_button_pressed():
 	print("host btn")
-	%MultiplayerHUD.hide()
+	%HUD.hide()
 	MultiplayerManager.become_host()
 
 
 func _join_button_pressed():
 	print("join btn")
-	%MultiplayerHUD.hide()
+	%HUD.hide()
 	MultiplayerManager.join_game()
