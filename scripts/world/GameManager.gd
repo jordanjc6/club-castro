4544800b9f93d0a59dfb5bab0a0fd48b13404717
Jordan Castro <jordanjcastro8@gmail.com>
2026-09-06 16:30:40 -0400

    only hide multiplayer HUD if success in hosting/joining lobby

--- scripts/world/GameManager.gd
+++ scripts/world/GameManager.gd
@@ -3,9 +3,6 @@ extends Node
 @onready var host_button: Button = $"../HUD/SideNav/MultiplayerHUD/VBoxContainer/HostButton"
 @onready var join_button: Button = $"../HUD/SideNav/MultiplayerHUD/VBoxContainer/JoinButton"
 
-var created_joinable_lobby = false  # for host
-var joined_lobby = false  # for joiners
-
 
 func _ready() -> void:
 	host_button.pressed.connect(_host_button_pressed)
@@ -13,15 +10,11 @@ func _ready() -> void:
 
 func _host_button_pressed():
 	print("host btn")
-	%HUD.hide()
-	
-	created_joinable_lobby = await MultiplayerManager.become_host()
+	if await MultiplayerManager.become_host(): %HUD.hide()
 
 
 func _join_button_pressed():
 	print("join btn")
-	%HUD.hide()
-	
-	var entered_code = "f453c9dcb52f4b1a85dcca79a1fb7c48"
+	var entered_code = "351938339bd044eebb1bc591d07e30f"
 	if entered_code != "":
-		joined_lobby = await MultiplayerManager.join_game(entered_code)
+		if await MultiplayerManager.join_game(entered_code): %HUD.hide()
