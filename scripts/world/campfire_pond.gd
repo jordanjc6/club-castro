9f05c5920d863ced74b5d1685b6d428b5c23d15c
Jordan Castro <jordanjcastro8@gmail.com>
2026-09-25 14:22:52 -0400

    add fishing area highlight, add rod unequip for singleplayer

--- scripts/world/campfire_pond.gd
+++ scripts/world/campfire_pond.gd
@@ -4,11 +4,15 @@ extends Node2D
 @export var exit_polygon: Polygon2D
 @export var exit_button:  Button
 
+@export var fishing_area_polygon: Polygon2D
+@export var fishing_area_button:  Button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# press bbt exit mat signal for animation
 	exit_button.pressed.connect(_on_exit_pressed)
+	fishing_area_button.pressed.connect(_on_fishing_area_pressed)
 
 func _on_exit_pressed() -> void:
 	var touch_pos = get_global_mouse_position()
@@ -17,10 +21,22 @@ func _on_exit_pressed() -> void:
 	var is_singleplayer = _move_local_player_to_position(touch_pos)
 	highlight_exit()
 
+func _on_fishing_area_pressed() -> void:
+	var touch_pos = get_global_mouse_position()
+	
+	# Move the player and check what type of character is currently active
+	var is_singleplayer = _move_local_player_to_position(touch_pos)
+	highlight_fishing_area()
+
 func highlight_exit():
 	var flash_tween = create_tween()
 	exit_polygon.modulate.a = 0.5
 	flash_tween.tween_property(exit_polygon, "modulate:a", 0.0, 0.25).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+func highlight_fishing_area():
+	var flash_tween = create_tween()
+	fishing_area_polygon.modulate.a = 0.5
+	flash_tween.tween_property(fishing_area_polygon, "modulate:a", 0.0, 0.25).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 
 func _move_local_player_to_position(target_pos: Vector2) -> bool:
 	var players = get_tree().get_nodes_in_group("player")
